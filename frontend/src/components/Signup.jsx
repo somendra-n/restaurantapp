d25b4d5e3c679ce59ff2d9669d332f189f77275e
somendra-n <152999461+somendra-n@users.noreply.github.com>
2025-04-03 20:58:46 +0530

    Update Signup.jsx

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -12,7 +12,7 @@ function Signup() {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://restaurantapp-backend-od0w.onrender.com/api/auth/register", {
+      const response = await fetch("http://localhost:5000/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, phoneNumber, password }), // ✅ Correct field names
