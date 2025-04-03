6c737ec15936b33a873f798e5a31705a9e116825
somendra-n <152999461+somendra-n@users.noreply.github.com>
2025-04-03 20:56:20 +0530

    Update FranchiseApplications.jsx

--- frontend/src/components/FranchiseApplications.jsx
+++ frontend/src/components/FranchiseApplications.jsx
@@ -6,7 +6,7 @@ function FranchiseApplications() {
 
   useEffect(() => {
     const fetchApplications = async () => {
-      const response = await fetch('https://restaurantapp-backend-od0w.onrender.com/api/franchise/applications');
+      const response = await fetch('http://localhost:5000/api/franchise/applications');
       const data = await response.json();
       setApplications(data);
       setLoading(false);
