42d537e8dbfab8a4042e28a35abad03910ca627d
somendra-n <152999461+somendra-n@users.noreply.github.com>
2025-04-03 20:57:52 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://restaurantapp-backend-od0w.onrender.com/api/auth/login", {
+      const response = await fetch("http://localhost:5000/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ phoneNumber, password }), // ✅ Correct field name
